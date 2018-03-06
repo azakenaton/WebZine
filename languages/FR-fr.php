c43a0a63f87d47ab20c1a9e852781d6350996e74
Cabrol Alexis <alexiscabrololivaux@gmail.com>
2018-03-06 17:04:27 +0100

    Ajout navbar

--- languages/FR-fr.php
+++ languages/FR-fr.php
@@ -1,1 +1,2 @@
 <?php
+define('TITRE',"Webzine");
---
+++ views/v_header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<title><?= TITRE ?></title>
+  	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="Language" content="<?= LANG ?>"/>
+	<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1; user-scalable=0"/>
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="<?= PATH_CSS ?>materialize.min.css" rel="stylesheet">
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="<?= PATH_SCRIPTS ?>materialize.min.js"></script>
+
+
+  </head>
+
+  
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
