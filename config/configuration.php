68be85ab25281008b297901ffb53fc7b05df05d6
Cabrol Alexis <alexiscabrololivaux@gmail.com>
2018-03-06 16:52:43 +0100

    Inital commit

---
+++ config/configuration.php
@@ -0,0 +1,34 @@
+<?php
+
+const DEBUG = true; // production : false; dev : true
+
+
+const BD_HOST = 'localhost';
+const BD_DBNAME = 'webzine';
+const BD_USER = 'root';
+const BD_PWD = '';
+// Langue du site
+const LANG ='FR-fr';
+
+// Paramètres du site : nom de l'auteur ou des auteurs
+const AUTEUR = 'webzine';
+
+//dossiers racines du site
+define('PATH_CONTROLLERS','./controllers/c_');
+define('PATH_ENTITY','./entities/');
+define('PATH_ASSETS','./assets/');
+define('PATH_MODELS','./models/');
+define('PATH_VIEWS','./views/v_');
+define('PATH_TEXTES','./languages/');
+
+//sous dossiers
+define('PATH_CSS', PATH_ASSETS.'css/');
+define('PATH_IMAGES', PATH_ASSETS.'images/');
+define('PATH_SCRIPTS', PATH_ASSETS.'js/');
+//define("PATH_GALERIE",'galerie/');
+//define('PATH_LOG','log/');
+
+//fichiers
+//define('LOG_BDD','logbdd.txt');
+define('PATH_LOGO', PATH_IMAGES.'logo.png');
+define('PATH_MENU', PATH_VIEWS.'menu.php');
